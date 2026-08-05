df36b5269fb3d81a418673d5ff1fde768c78888a
Leviathan19 <msnarvaez19@hotmail.com>
2026-08-04 23:20:23 -0500

    feat: :white_check_mark: Se añadió el archivo AppNavigator.tsx

---
+++ app/navigation/AppNavigator.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useAppInit } from '../hooks/useAppInit';
+import { ValidationWhitelist } from '../views/Validation_whitelist';
+import { CheckSync } from '../views/Check_sync';
+import { MainMenu } from '../views/Main_menu';
+import { ClientesList } from '../views/Clientes';
+import { ActivityIndicator, View } from 'react-native';
+
+const Stack = createNativeStackNavigator();
+
+export const AppNavigator = () => {
+  const { appState, setAppState, deviceId, checkDeviceStatus } = useAppInit();
+
+  if (appState === 'loading') {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {appState === 'main_menu' ? (
+          // Stack Principal
+          <>
+            <Stack.Screen name="MainMenu" component={MainMenu} />
+            <Stack.Screen name="Clientes" component={ClientesList} />
+          </>
+        ) : appState === 'active' ? (
+          // Stack Sincronización Inicial
+          <Stack.Screen name="CheckSync">
+            {(props) => <CheckSync {...props} deviceId={deviceId} setAppState={setAppState} />}
+          </Stack.Screen>
+        ) : (
+          // Stack Validación
+          <Stack.Screen name="Validation">
+            {(props) => (
+              <ValidationWhitelist 
+                {...props}
+                appState={appState} 
+                deviceId={deviceId} 
+                setAppState={setAppState} 
+                checkDeviceStatus={checkDeviceStatus} 
+              />
+            )}
+          </Stack.Screen>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
