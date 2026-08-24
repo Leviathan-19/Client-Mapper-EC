cb57d683852a8f4077189e444d4ae9cff53ba33d
Leviathan19 <msnarvaez19@hotmail.com>
2026-08-24 11:14:26 -0500

    feat: :white_check_mark: Se agregó el modulo "Agendamiento" al menú
    El mismo que permitirá hacer todo los de los modulos anteriores en uno solo, esta en fase de pruebas, pendiente por corregir algunos detalles de la interfaz

--- app/navigation/AppNavigator.tsx
+++ app/navigation/AppNavigator.tsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useAppInit } from '../hooks/useAppInit';
-import { ValidationWhitelist } from '../views/Validation_whitelist';
-import { CheckSync } from '../views/Check_sync';
-import { MainMenu } from '../views/Main_menu';
-import { ClientesList } from '../views/Main_menu/Customers';
-import { RoutesList } from '../views/Main_menu/Routes';
-import { RouteDetail } from '../views/Main_menu/Routes/Detail';
-import { ProductsList } from '../views/Main_menu/Products';
-import { VisitsList } from '../views/Main_menu/Visits';
-import { ActivityIndicator, View } from 'react-native';
-import { ProfileNavbar } from '../views/Main_menu/ProfileNavbar';
-import { SqlRunner } from '../views/Main_menu/SqlRunner';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useAppInit } from "../hooks/useAppInit";
+import { ValidationWhitelist } from "../views/Validation_whitelist";
+import { CheckSync } from "../views/Check_sync";
+import { MainMenu } from "../views/Main_menu";
+import { ClientesList } from "../views/Main_menu/Customers";
+import { RoutesList } from "../views/Main_menu/Routes";
+import { RouteDetail } from "../views/Main_menu/Routes/Detail";
+import { ProductsList } from "../views/Main_menu/Products";
+import { VisitsList } from "../views/Main_menu/Visits";
+import { AgendamientoScreen } from "../views/Main_menu/Agendamiento";
+import { ActivityIndicator, View } from "react-native";
+import { ProfileNavbar } from "../views/Main_menu/ProfileNavbar";
+import { SqlRunner } from "../views/Main_menu/SqlRunner";
 
 const Stack = createNativeStackNavigator();
 
 export const AppNavigator = () => {
   const { appState, setAppState, deviceId, checkDeviceStatus } = useAppInit();
 
-  if (appState === 'loading') {
+  if (appState === "loading") {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <ActivityIndicator size="large" color="#0000ff" />
       </View>
     );
@@ -31,39 +32,49 @@ export const AppNavigator = () => {
     <View style={{ flex: 1 }}>
       <NavigationContainer>
         <View style={{ flex: 1 }}>
-          {appState === 'main_menu' && <ProfileNavbar />}
+          {appState === "main_menu" && <ProfileNavbar />}
           <Stack.Navigator screenOptions={{ headerShown: false }}>
-          {appState === 'main_menu' ? (
-            // Stack Principal
-            <>
-              <Stack.Screen name="MainMenu" component={MainMenu} />
-              <Stack.Screen name="Clientes" component={ClientesList} />
-              <Stack.Screen name="Routes" component={RoutesList} />
-              <Stack.Screen name="RouteDetail" component={RouteDetail} />
-              <Stack.Screen name="Productos" component={ProductsList} />
-              <Stack.Screen name="Visitas" component={VisitsList} />
-              <Stack.Screen name="SqlRunner" component={SqlRunner} />
-            </>
-          ) : appState === 'active' ? (
-            // Stack Sincronización Inicial
-            <Stack.Screen name="CheckSync">
-              {(props) => <CheckSync {...props} deviceId={deviceId} setAppState={setAppState} />}
-            </Stack.Screen>
-          ) : (
-            // Stack Validación
-            <Stack.Screen name="Validation">
-              {(props) => (
-                <ValidationWhitelist 
-                  {...props}
-                  appState={appState} 
-                  deviceId={deviceId} 
-                  setAppState={setAppState} 
-                  checkDeviceStatus={checkDeviceStatus} 
+            {appState === "main_menu" ? (
+              // Stack Principal
+              <>
+                <Stack.Screen name="MainMenu" component={MainMenu} />
+                <Stack.Screen name="Clientes" component={ClientesList} />
+                <Stack.Screen name="Routes" component={RoutesList} />
+                <Stack.Screen name="RouteDetail" component={RouteDetail} />
+                <Stack.Screen name="Productos" component={ProductsList} />
+                <Stack.Screen name="Visitas" component={VisitsList} />
+                <Stack.Screen
+                  name="Agendamiento"
+                  component={AgendamientoScreen}
                 />
-              )}
-            </Stack.Screen>
-          )}
-        </Stack.Navigator>
+                <Stack.Screen name="SqlRunner" component={SqlRunner} />
+              </>
+            ) : appState === "active" ? (
+              // Stack Sincronización Inicial
+              <Stack.Screen name="CheckSync">
+                {(props) => (
+                  <CheckSync
+                    {...props}
+                    deviceId={deviceId}
+                    setAppState={setAppState}
+                  />
+                )}
+              </Stack.Screen>
+            ) : (
+              // Stack Validación
+              <Stack.Screen name="Validation">
+                {(props) => (
+                  <ValidationWhitelist
+                    {...props}
+                    appState={appState}
+                    deviceId={deviceId}
+                    setAppState={setAppState}
+                    checkDeviceStatus={checkDeviceStatus}
+                  />
+                )}
+              </Stack.Screen>
+            )}
+          </Stack.Navigator>
         </View>
       </NavigationContainer>
     </View>
